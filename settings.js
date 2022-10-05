37db407ab55892def7af57271797a55c5b17a317
SAYU_BOT <110476489+ravishka2@users.noreply.github.com>
2022-10-05 13:59:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,30 +36,30 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
+global.vcardowner = ['94760559964'] //ur owner number
 global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94760559964']
+global.ownertag = '94760559964' //ur tag number
+global.botnumber = '94760559964' //Add your Bot Number For Log
+global.botname = 'AdZone_ADVERTISING BOT (❤️SAYU❤️ R+S)' //ur bot name
+global.ig = "https://www.instagram.com/ravishka__sathsara/" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCYe800QkSFht8OnNU13ms8g" //ur website to be displayed
+global.sc = 'https://github.com/ravishka2' //script link
+global.linkz = 'https://www.youtube.com/channel/UCYe800QkSFht8OnNU13ms8g'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.packname = "AdZone_ADVERTISING BOT (❤️SAYU❤️ R+S" //ur sticker watermark packname
+global.author = "AdZone_ADVERTISING RAVISHKA❤️S❤️" //ur sticker watermark author
+global.footer = "® AdZone_ADVERTISING RAVISHKA❤️S❤️ ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.aliveimg ="https://telegra.ph/file/8924798a5171d4d94cb4b.jpg"
+global.allmenuimg ="https://telegra.ph/file/8924798a5171d4d94cb4b.jpg"
+global.welcomeimg= "https://telegra.ph/file/8924798a5171d4d94cb4b.jpg"
+global.gbyeimg = "https://telegra.ph/file/8924798a5171d4d94cb4b.jpg"
 global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94760559964'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
